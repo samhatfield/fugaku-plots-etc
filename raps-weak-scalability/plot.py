295fa810ea4a40e3a03d3bc3d11a9947f802557f
Sam Hatfield <samuel.hatfield@ecmwf.int>
2022-05-19 09:10:31 +0000

    Update raps-weak-scalability

--- raps-weak-scalability/plot.py
+++ raps-weak-scalability/plot.py
@@ -24,19 +24,19 @@ data = pd.read_csv("data.csv")
 data["Truncation"] = [int(res[3:]) for res in data["Resolution"]]
 data["Resolution (km)"] = [ceil(km_from_tco(tco)) for tco in data["Truncation"]]
 
-fig, axes = plt.subplots(figsize=(6,4))
-axes.plot(data["Truncation"], data["SDPD"])
+fig, axes = plt.subplots(figsize=(8,4))
+axes.plot(data["Truncation"], data["SDPD"], 'x-')
 
 axes.grid(False)
 
-xlims = [0, 1400]
-ylims = [200, 350]
+xlims = [0, 2580]
+ylims = [100, 350]
 
 axes.plot(xlims, [240, 240], '--')
 axes.text(xlims[1]+40, 240, "Operational target\n(10 days in 1 hour)", verticalalignment="center")
 
 axes.plot([1279, 1279], ylims, ':')
-axes.text(1279, 365, "Operational resolution", horizontalalignment="center")
+axes.text(1279, 385, "Operational\nresolution", horizontalalignment="center")
 
 axes.set_xlim(xlims)
 axes.set_ylim(ylims)
@@ -47,10 +47,18 @@ ax2.set_xlim(xlims)
 ax2.set_xticks(data["Truncation"])
 ax2.set_xticklabels([f"{tco} km" for tco in data["Resolution (km)"]])
 
-for nodes, trunc, sdpd in zip(data["Nodes"], data["Truncation"], data["SDPD"]):
-    text = f"{nodes}\nnode" if nodes == 1 else f"{nodes}\nnodes"
-    axes.text(trunc, sdpd+7, text, horizontalalignment="center", backgroundcolor="white",
-              bbox=dict(boxstyle='square,pad=0.1', fc='white', ec='none'))
+for i, (nodes, trunc, sdpd) in enumerate(zip(data["Nodes"], data["Truncation"], data["SDPD"])):
+    if i == 0:
+        text = f"{nodes}\nnode" if nodes == 1 else f"{nodes}\nnodes"
+    else:
+        text = f"{nodes}"
+
+    # The text for 1279 needs to be visible above the green-dashed line so the alpha is set to 1.0
+    # for that datapoint alone
+    alpha = 1.0 if trunc == 1279 else 0.0
+
+    axes.text(trunc, sdpd+13, text, horizontalalignment="center", backgroundcolor="white",
+              bbox=dict(boxstyle='square,pad=0.1', fc='white', ec='none', alpha=alpha))
 
 axes.set_xlabel("Spectral truncation")
 axes.set_ylabel("Simulated days per day")
